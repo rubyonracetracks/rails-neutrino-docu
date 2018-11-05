044c5fb0ca8bdae86eb5c129c0d420a81a99a8f2
Jason Hsu <jhsu802701@jasonhsu.com>
2018-11-05 03:15:35 +0000

    Removed doc3

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -40,9 +40,6 @@ class Footer extends React.Component {
             <a href={this.docUrl('doc2.html', this.props.language)}>
               Guides (or other categories)
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
-            </a>
           </div>
           <div>
             <h5>Community</h5>
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,7 +37,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
-    {page: 'help', label: 'Help'}
+    {page: 'help', label: 'Help'},
   ],
 
   // If you have users set above, you add it here:
